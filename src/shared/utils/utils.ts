8d4eea800b2594be83cefeea9a64368a66b5829f
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-01 20:11:16 -0300

    Se modificó la ubicación del utils a la carpeta shared

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,4 +1,4 @@
-import type { RoomEventDetailsDTO } from "../../shared/types/types";
+import type { RoomEventDetailsDTO } from "../types/types";
 import { isAxiosError } from "axios";
 
 export const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
