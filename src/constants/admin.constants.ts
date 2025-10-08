d973db81f7710590321ee03d26a2b531062803fb
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-08 17:33:07 -0300

    Se agregan constantes para admin

--- src/constants/admin.constants.ts
+++ src/constants/admin.constants.ts
@@ -1,9 +1,9 @@
 export const ADMIN_ENDPOINTS = {
   getAllEventsAdmin: () => `/calendar/events`, 
-  getLogs: () => `/logs`, 
+  getLogs: () => `/logs`, // TODO: A definir con backend endpoint correcto
 } as const;
 
 export const ADMIN_ERROR_MESSAGES = {
   eventsError: "No se pudo obtener la agenda de la sala.",
-  unknownError: "Error desconocido",
+  logsError: "No se pudieron cargar los logs del sistema.",
 } as const;
