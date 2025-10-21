47f68100d683183e8ee4e105feafef394f804b13
gusV15 <gustavov0706@gmail.com>
2025-10-20 23:55:41 -0300

    feat: se agregan estilos y constantes para la HomePage

---
+++ src/pages/HomePage/styles.ts
@@ -0,0 +1,147 @@
+import styled, { css } from "styled-components";
+
+export const ROOM_PAGE_COLORS = {
+  roomTitle: "#0f172a",
+  roomTitleFree: "#16a249",
+  roomTitleOccupied: "#ef4343",
+  roomText: "#64748b",
+  roomBorder: "#e5e7eb",
+  roomBoxShadow: "#0000001a",
+};
+
+export const HomePageStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const RoomStatusContainer = styled.section`
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  justify-content: center;
+  width: 90%;
+  div:has(> select) {
+    width: 400px;
+  }
+`;
+
+export const RoomListContainer = styled(RoomStatusContainer)`
+  flex-wrap: wrap;
+`;
+
+export const SelectFilterContainer = styled(RoomStatusContainer)`
+  justify-content: flex-start;
+  aligmn-items: center;
+`;
+
+export const AllRoomsCardContainerStyles = css`
+  border-radius: 0.75rem;
+  border-width: 1px;
+  border-color: ${ROOM_PAGE_COLORS.roomBorder};
+  box-shadow: 0 1px 2px ${ROOM_PAGE_COLORS.roomBoxShadow};
+  padding: 1.5rem;
+  h2 {
+    color: ${ROOM_PAGE_COLORS.roomTitle};
+    margin: 0 0 0.5rem;
+    font-weight: bold;
+  }
+  p {
+    color: ${ROOM_PAGE_COLORS.roomText};
+    margin: 0;
+  }
+`;
+
+export const FreeRoomsCardContainerStyles = css`
+  border-radius: 0.75rem;
+  border-width: 1px;
+  border-color: ${ROOM_PAGE_COLORS.roomBorder};
+  box-shadow: 0 1px 2px ${ROOM_PAGE_COLORS.roomBoxShadow};
+  padding: 1.5rem;
+  h2 {
+    color: ${ROOM_PAGE_COLORS.roomTitleFree};
+    margin: 0 0 0.5rem;
+    font-weight: bold;
+  }
+  p {
+    color: ${ROOM_PAGE_COLORS.roomText};
+    margin: 0;
+  }
+`;
+
+export const OccupiedRoomsCardContainerStyles = css`
+  border-radius: 0.75rem;
+  border-width: 1px;
+  border-color: ${ROOM_PAGE_COLORS.roomBorder};
+  box-shadow: 0 1px 2px ${ROOM_PAGE_COLORS.roomBoxShadow};
+  padding: 1.5rem;
+  h2 {
+    color: ${ROOM_PAGE_COLORS.roomTitleOccupied};
+    margin: 0 0 0.5rem;
+    font-weight: bold;
+  }
+  p {
+    color: ${ROOM_PAGE_COLORS.roomText};
+    margin: 0;
+  }
+`;
+export const RoomListContainerStyles = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border-radius: 0.75rem;
+  border: 1px solid ${ROOM_PAGE_COLORS.roomBorder};
+  box-shadow: 0 1px 2px ${ROOM_PAGE_COLORS.roomBoxShadow};
+  padding: 1.5rem;
+  width: 28%;
+  p {
+    color: ${ROOM_PAGE_COLORS.roomText};
+    margin: 0;
+  }
+`;
+
+export const RoomStatusSectionStyles = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h2 {
+    font-size: 1.2rem;
+    color: ${ROOM_PAGE_COLORS.roomTitle};
+    margin: 0 0 0.5rem;
+    font-weight: bold;
+  }
+`;
+
+export const RoomInfoSectionStyles = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+`;
+
+export const RoomEventsSectionStyles = styled.div`
+  width: 100%;
+`;
+
+export const RoomEventItemStyles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+  margin-top: 1rem;
+  p {
+    margin: 0;
+    font-weight: bold;
+  }
+`;
+
+export const RoomEventTimeSectionStyles = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  p {
+    margin: 0;
+    font-weight: normal;
+  }
+`;
