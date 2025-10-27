86ee3587311fbf3316e99d88694cbf97c8e2207c
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-27 16:35:24 -0300

    BackLink compartido en todas las pantallas (provisional). Luego hay que generarlo como un componente en shared para que tenga estilos en cada pantalla específica.

--- src/routes/FinnegansRoutes.tsx
+++ src/routes/FinnegansRoutes.tsx
@@ -1,18 +1,29 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import AdminEvents from "../pages/AdminEvents/AdminEventsPage"
-import { AdminLogs } from "../pages/AdminLogs/AdminLogsPage"
+import { AdminLogsPage } from "../pages/AdminLogs/AdminLogsPage"
 import { HomePage } from "../pages/HomePage"
 import { RoomPage } from "../pages/RoomPage/RoomPage"
 import Header from "../shared/components/Header/Header"
+import { BackLink } from "../pages/RoomPage/stylesRoomPage"
+import { ArrowLeft } from "lucide-react"
 
 export const FinnegansRoutes = () => {
+
+    const navigate = useNavigate()
+
     return (
         <>
             <Header />
+            {window.location.pathname !== "/home" && (
+                <BackLink onClick={() => navigate("/home")}>
+                    <ArrowLeft size={18} />
+                    Volver
+                </BackLink>
+            )}
             <Routes>
                 <Route path="/home" element={<HomePage />} />
                 <Route path="/room/:id" element={<RoomPage />} />
-                <Route path="/admin/logs" element={<AdminLogs />} />
+                <Route path="/admin/logs" element={<AdminLogsPage />} />
                 <Route path="/admin/events" element={<AdminEvents />} />
 
                 <Route path="/*" element={<div>Finnegans Routes</div>} />
