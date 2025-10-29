54b7c54a53bd849b0f81319d1ba7680078d09c82
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-28 22:57:57 -0300

    Se modifica lógica para incorporar al contexto y localStorage el email del usuario loggeado. Esto servirá para enviarlo en el CheckInCurrentEvent.

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -1,22 +1,26 @@
 import { useState, type ReactNode } from "react";
-
 import { AuthContext } from "./authContext";
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [logged, setLogged] = useState(!!localStorage.getItem("token"));
+    const [logged, setLogged] = useState(() => !!localStorage.getItem("token"));
+    const [userEmail, setUserEmail] = useState<string | null>(localStorage.getItem("userEmail"));
 
-    const login = (token: string) => {
+    const login = (token: string, email: string) => {
         localStorage.setItem("token", token);
+        localStorage.setItem("userEmail", email);
         setLogged(true);
+        setUserEmail(email);
     };
 
     const logout = () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("userEmail");
         setLogged(false);
+        setUserEmail(null);
     };
 
     return (
-        <AuthContext.Provider value={{ logged, login, logout }}>
+        <AuthContext.Provider value={{ logged, userEmail, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
