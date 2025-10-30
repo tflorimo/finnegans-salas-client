e4c4cb47f36ea0749a8bc6da0fbf9b832f781306
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-30 15:42:09 -0300

    Refactor axiosInstance para utilizar constantes y helpers

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -5,10 +5,13 @@ import axios, {
   type InternalAxiosRequestConfig,
 } from "axios";
 import { getAuthToken } from "../context/auth/utils";
-import { authService } from "./auth/auth.service";
+  import { authService } from "./auth/auth.service";
 
 const DEFAULT_BASE_URL = import.meta.env.VITE_BASE_URL;
 const DEFAULT_TIMEOUT = import.meta.env.VITE_TIMEOUT || 5000;
+const AUTHORIZATION_HEADER = "Authorization";
+const SKIP_REFRESH_HEADER = "x-skip-auth-refresh";
+const BEARER_PREFIX = "Bearer";
 
 type RetriableConfig = InternalAxiosRequestConfig & { _retry?: boolean };
 
@@ -24,57 +27,56 @@ const shouldSkipAuthRefresh = (
 ): boolean => {
   if (!headers) return false;
   if (headers instanceof AxiosHeaders) {
-    return headers.get("x-skip-auth-refresh") === "true";
+    return headers.get(SKIP_REFRESH_HEADER) === "true";
   }
-  return headers["x-skip-auth-refresh"] === "true";
+  return headers[SKIP_REFRESH_HEADER] === "true";
 };
 
-const withBearer = (
+const setAuthorizationHeader = (
   headers: AxiosRequestHeaders | AxiosHeaders | undefined,
   token: string,
 ): AxiosRequestHeaders | AxiosHeaders => {
   if (headers instanceof AxiosHeaders) {
-    headers.set("Authorization", `Bearer ${token}`);
+    headers.set(AUTHORIZATION_HEADER, `${BEARER_PREFIX} ${token}`);
     return headers;
   }
 
-  const merged = AxiosHeaders.from(headers ?? {});
-  merged.set("Authorization", `Bearer ${token}`);
-  return merged;
+  const normalizedHeaders = AxiosHeaders.from(headers ?? {});
+  normalizedHeaders.set(AUTHORIZATION_HEADER, `${BEARER_PREFIX} ${token}`);
+  return normalizedHeaders;
 };
 
-axiosInstance.interceptors.request.use((config) => {
+const attachTokenToRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
   const token = getAuthToken();
   if (token) {
-    config.headers = withBearer(config.headers, token);
+    config.headers = setAuthorizationHeader(config.headers, token);
   }
   return config;
-});
+};
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error: AxiosError) => {
-    const { response, config } = error;
-    const originalConfig = config as RetriableConfig | undefined;
+const handleResponseError = async (error: AxiosError) => {
+  const { response, config } = error;
+  const originalConfig = config as RetriableConfig | undefined;
 
-    if (
-      response?.status === 401 &&
-      originalConfig &&
-      !originalConfig._retry &&
-      !shouldSkipAuthRefresh(originalConfig.headers)
-    ) {
-      originalConfig._retry = true;
+  if (
+    response?.status === 401 &&
+    originalConfig &&
+    !originalConfig._retry &&
+    !shouldSkipAuthRefresh(originalConfig.headers)
+  ) {
+    originalConfig._retry = true;
 
-      const newToken = await authService.refreshAccessToken();
-
-      if (newToken) {
-        originalConfig.headers = withBearer(originalConfig.headers, newToken);
-        return axiosInstance(originalConfig);
-      }
+    const newToken = await authService.refreshAccessToken();
+    if (newToken) {
+      originalConfig.headers = setAuthorizationHeader(originalConfig.headers, newToken);
+      return axiosInstance(originalConfig);
     }
+  }
 
-    return Promise.reject(error);
-  },
-);
+  return Promise.reject(error);
+};
+
+axiosInstance.interceptors.request.use(attachTokenToRequest);
+axiosInstance.interceptors.response.use((response) => response, handleResponseError);
 
 export default axiosInstance;
