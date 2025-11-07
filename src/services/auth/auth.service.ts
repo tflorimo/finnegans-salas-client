a0ecbdd511e75224f0e1883f91f44744f7378c29
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-06 23:48:22 -0300

    Correciones de auth.service

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { getErrorMessage } from "../../api/axios/axios.utils";
 import type { AuthResponse } from "./types";
 import axiosInstance from "../../api/axios/axios.instance";
 import type { AxiosRequestConfig } from "axios";
-import { clearAuthToken, clearStoredUserEmail, setAuthToken } from "../../context/auth/utils";
+import { clearAuthToken, clearStoredUserEmail, setAuthToken } from "../../shared/utils/localStorage.utils";
 
 const AUTH_HEADER = (token: string) => `Bearer ${token}`;
 const REFRESH_REQUEST_CONFIG: AxiosRequestConfig = {
@@ -27,6 +27,9 @@ export const authService = {
   logoutApp: async (): Promise<void> => {
     try {
       await axiosInstance.post(AUTH_ENDPOINTS.logoutApp());
+    } catch (error) {
+      const message = getErrorMessage(error, ROOM_ERROR_MESSAGES.logoutError);
+      throw new Error(message);
     } finally {
       // Aunque falle la llamada, vaciamos la sesión del front.
       clearAuthToken();
@@ -34,26 +37,13 @@ export const authService = {
       delete axiosInstance.defaults.headers.common.Authorization;
     }
   },
-  // TODO: Se implementará para solicitar datos iniciales del usuario (Ya está implementado en)
-  /* checkAuth: async (): Promise<{ user: AuthUser; authenticated: boolean }> => {
-    try {
-      const { data } = await axiosInstance.get<{
-        user: AuthUser;
-        authenticated: boolean;
-      }>(AUTH_ENDPOINTS.checkAuth());
-      return data;
-    } catch (error) {
-      const message = getErrorMessage(error, ROOM_ERROR_MESSAGES.roomError);
-      throw new Error(message);
-    }
-  }, */
 
   refreshAccessToken: async (): Promise<string | null> => {
     try {
       const { data } = await axiosInstance.post<{ accessToken: string | null }>(
         AUTH_ENDPOINTS.refresh(),
-        {}, 
-        REFRESH_REQUEST_CONFIG, 
+        {},
+        REFRESH_REQUEST_CONFIG,
       );
 
       const newToken = data.accessToken ?? null;
@@ -74,5 +64,4 @@ export const authService = {
       return null;
     }
   },
-
 };
