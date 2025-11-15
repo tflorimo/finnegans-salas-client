40107c058ffa20ae840280a8f36b68b5732cfe3d
LeandroCTaddeo <leandrotaddeo03@gmail.com>
2025-11-15 14:33:14 -0300

    ajusto ReservationItemComponent

--- src/pages/RoomPage/components/ReservationItemComponent.tsx
+++ src/pages/RoomPage/components/ReservationItemComponent.tsx
@@ -3,6 +3,8 @@ import { Avatar, ResInfo, ResLeft, ResRight, ReservationItem } from "../styles";
 import { initials } from "../utils/RoomPageUtils";
 import { getDayName } from "../utils/RoomPageUtils";
 import { truncateTextByLength } from "../../../shared/utils/text.utils";
+import { ThemeContext } from "../../../context/theme/themeContext";
+import { useContext } from "react";
 interface ReservationItemComponentProps {
   organizer: string;
   start: string | Date | undefined;
@@ -10,7 +12,6 @@ interface ReservationItemComponentProps {
   date: Date | string;
   title: string;
 }
-
 export const ReservationItemComponent = ({
   organizer,
   start,
@@ -18,18 +19,19 @@ export const ReservationItemComponent = ({
   date,
   title,
 }: ReservationItemComponentProps) => {
+  const {theme} = useContext(ThemeContext);
   return (
     <ReservationItem>
       <ResLeft>
         <Avatar>{initials(organizer)}</Avatar>
-        <ResInfo>
+        <ResInfo $theme={theme}>
           <span>
             {organizer} - {truncateTextByLength(title, 30)}
           </span>
           <small>{getDayName(date)}</small>
         </ResInfo>
       </ResLeft>
-      <ResRight>
+      <ResRight  $theme={theme}>
         <Clock size={16} />
         <span>{`${start ?? '-'} - ${end ?? '-'}`}</span>
       </ResRight>
