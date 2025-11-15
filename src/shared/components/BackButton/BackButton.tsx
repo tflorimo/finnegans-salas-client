ad24c9b5648bdaca2bb1f13d8a3ba494762670b9
LeandroCTaddeo <leandrotaddeo03@gmail.com>
2025-11-15 14:33:40 -0300

    ajusto BackButton

--- src/shared/components/BackButton/BackButton.tsx
+++ src/shared/components/BackButton/BackButton.tsx
@@ -1,12 +1,15 @@
 import { ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { BackButtonStyled } from "./styles";
+import { ThemeContext } from "../../../context/theme/themeContext";
+import { useContext } from "react";
 
 export const BackButton = () => {
   const navigate = useNavigate();
-
+  const {theme} = useContext(ThemeContext);
+  
   return (
-    <BackButtonStyled onClick={() => navigate("/home")}>
+    <BackButtonStyled $theme={theme} onClick={() => navigate("/home")}>
       <ArrowLeft size={18} />
       Volver al inicio
     </BackButtonStyled>
