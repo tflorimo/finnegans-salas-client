d2f35f2ce55395d5653363e40740eabddb11a74d
LeandroCTaddeo <leandrotaddeo03@gmail.com>
2025-11-19 23:47:02 -0300

    Se realizan correcciones para la compatiblidad entre cambios realizados.

--- src/pages/RoomPage/components/ReservationItemComponent.tsx
+++ src/pages/RoomPage/components/ReservationItemComponent.tsx
@@ -1,16 +1,20 @@
-import { Clock } from "lucide-react";
-import { Avatar, ResInfo, ResLeft, ResRight, ReservationItem } from "../styles";
-import { initials } from "../utils/RoomPageUtils";
-import { getDayName } from "../utils/RoomPageUtils";
+import { Avatar, ResInfo, ResRight, ReservationItem, FinishedEventIcon, InProgressEventIcon } from "../styles";
+import { initials, getDayName } from "../utils/RoomPageUtils";
 import { truncateTextByLength } from "../../../shared/utils/text.utils";
 import { ThemeContext } from "../../../context/theme/themeContext";
 import { useContext } from "react";
+import { EventStatusIcon } from "../../../shared/components/EventStatusIcon";
+
 interface ReservationItemComponentProps {
   organizer: string;
   start: string | Date | undefined;
   end: string | Date | undefined;
   date: Date | string;
   title: string;
+  endTime: Date | string; 
+  startTime: Date | string;
+  eventId: string;
+  currentEventId?: string;
 }
 export const ReservationItemComponent = ({
   organizer,
@@ -18,21 +22,31 @@ export const ReservationItemComponent = ({
   end,
   date,
   title,
+  endTime,
+  startTime,
+  eventId,
+  currentEventId,
 }: ReservationItemComponentProps) => {
   const {theme} = useContext(ThemeContext);
   return (
     <ReservationItem>
-      <ResLeft>
-        <Avatar>{initials(organizer)}</Avatar>
-        <ResInfo $theme={theme}>
-          <span>
-            {organizer} - {truncateTextByLength(title, 30)}
-          </span>
-          <small>{getDayName(date)}</small>
-        </ResInfo>
-      </ResLeft>
-      <ResRight  $theme={theme}>
-        <Clock size={16} />
+      <Avatar>{initials(organizer)}</Avatar>
+      <ResInfo $theme={theme}>
+        <span>
+          {organizer} - {truncateTextByLength(title, 50)}
+        </span>
+        <small>{getDayName(date)}</small>
+      </ResInfo>
+      <ResRight $theme={theme}>
+        <EventStatusIcon
+          startTime={startTime}
+          endTime={endTime}
+          eventId={eventId}
+          currentEventId={currentEventId}
+          size={16}
+          FinishedWrapper={FinishedEventIcon}
+          InProgressWrapper={InProgressEventIcon}
+        />
         <span>{`${start ?? '-'} - ${end ?? '-'}`}</span>
       </ResRight>
     </ReservationItem>
