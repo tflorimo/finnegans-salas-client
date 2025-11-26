da93d14f2ae42151fbfe7525048641330a1ec9cb
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-26 20:14:21 -0300

    feat: Implementar página de Not Found con mensajes y navegación

---
+++ src/pages/NotFoundPage/constants/NotFoundPage.constants.ts
@@ -0,0 +1,10 @@
+export const NOT_FOUND_MESSAGES = {
+  TITLE: "Página no encontrada",
+  DESCRIPTION: "Lo sentimos, la página que estás buscando no existe o ha sido movida.",
+  BUTTON_TEXT: "Volver al inicio",
+  ERROR_CODE: "404",
+};
+
+export const NOT_FOUND_ROUTES = {
+  HOME: "/home",
+};
