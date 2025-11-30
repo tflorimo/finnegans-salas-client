6a27c147d7e7399be3a1a0d6d942dbc151831d47
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-30 11:48:55 -0300

    Se limpian imports innecesarios

--- src/FinnegansApp.tsx
+++ src/FinnegansApp.tsx
@@ -1,7 +1,5 @@
-import { useContext } from "react";
 import { Layout } from "./components/Layout/Layout";
 import { AuthProvider } from "./context/auth/AuthProvider";
-import { ThemeContext } from "./context/theme/themeContext";
 import { ThemeProvider } from "./context/theme/themeProvider";
 import { AppRouter } from "./router/AppRouter";
 
