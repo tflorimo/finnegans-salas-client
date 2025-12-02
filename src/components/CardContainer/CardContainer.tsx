4acfad69d01d9c0ce1c5a6c35d3056cdc132a00e
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-12-02 15:51:32 -0300

    Se corrige horizontalidadel código y la carpeta shared/Styles/media a: shared/styles/media

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -5,5 +5,11 @@ import { ThemeContext } from "../../context/theme/themeContext";
 
 export function CardContainer({ children, customStyle, onClick }: CardContainerProps) {
     const { theme } = useContext(ThemeContext);
-    return <CardContainerStyled $customStyle={customStyle} $theme={theme} onClick={onClick && onClick}>{children}</CardContainerStyled>
+    return <CardContainerStyled
+        $customStyle={customStyle}
+        $theme={theme}
+        onClick={onClick && onClick}
+    >
+        {children}
+    </CardContainerStyled>
 }
